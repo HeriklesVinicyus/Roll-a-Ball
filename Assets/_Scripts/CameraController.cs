b78788b495e69b9766af16d6b29d1398c59fb153
Hérikles Vinícyus <heriklesvinicyus@hotmail.com>
2018-04-25 20:58:01 -0300

    Jogavel

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	void Start (){
+		offset = transform.position - player.transform.position;
+	}
+
+	//Chamado como o Update, porém é quando todos dos outros updates são chamados
+	void LateUpdate (){
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public int score;
+	public  Text scoreText;
+	public  Text winText;
+	public float speed;
+	private Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		updateScore ();
+		winText.text = "";
+	}
+		
+	// FixedUpdate é chamado quando ocorre um novo calculo na física
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");	
+
+		Vector3 moviment = new Vector3 (moveHorizontal, 0f, moveVertical);
+
+		rb.AddForce (moviment * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive (false);
+			score++;
+			updateScore ();
+		}
+	}
+
+	void updateScore(){
+		scoreText.text= "Score: "+this.score;
+		if(score>=8){
+			winText.text = "You Win!";
+		}
+	}
+
+}
